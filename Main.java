034e251ba23eed9b921ffff1cbd4ecff6f54fbc1
TomLucasakaTGeek <120933239+TomLucasakaTGeek@users.noreply.github.com>
2023-06-15 15:16:15 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    
+    System.out.println("7 8 9 X");
+    System.out.println("4 5 6 -");
+    System.out.println("1 2 3 +");
+    System.out.println("0 ^ / =");
+    int ans=0,a,b;System.out.println();
+    System.out.println("Press '+' to add the two numbers");
+    System.out.println("Press '-' to subtract the two numbers");
+    System.out.println("Press 'X' to multiply the two numbers");
+    System.out.println("Press '/' to divide the two numbers");
+    System.out.println("Press '^' to take index value");
+    Scanner o = new Scanner(System.in);
+    char s = o.next().charAt(0);
+  switch(s){
+    case '+': System.out.println("Enter first number");a=o.nextInt();
+              System.out.println("Enter second number");b=o.nextInt();
+                ans = a+b;
+                break;
+      case '-':System.out.println("Enter first number");a=o.nextInt();
+              System.out.println("Enter second number");b=o.nextInt();
+                ans = a-b;
+                break;
+      case 'X':System.out.println("Enter first number");a=o.nextInt();
+              System.out.println("Enter second number");b=o.nextInt();
+                ans = a*b;
+                break;
+      case '/':System.out.println("Enter first number");a=o.nextInt();
+              System.out.println("Enter second number");b=o.nextInt();
+                ans = (int)a/b;
+                break;
+      case '^':System.out.println("Enter first number");a=o.nextInt();
+              System.out.println("Enter second number");b=o.nextInt();
+                ans = (int)Math.pow(a,b);
+                break;
+      default : System.exit(0);
+      } System.out.println("\n\n"+ans);
+ }
+}
